93ea013a96ad5fd6166b08147804e03b3a311561
Pierre DELGRANGE <pierre.delgrange@etudiant.univ-lille1.fr>
2018-09-27 19:46:05 +0200

    tp3 exercice 1 2 3

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -1,2 +1,60 @@
 #include <stdio.h>
 #include "tp3.h"
+
+int mon_strlen(char s[]){
+
+	int i = 0;
+
+	while(s[i] != '\0'){
+		i++;
+	}
+	return i;
+}
+
+int mon_strlen2(const char *s){
+
+	int i = 0;
+
+	while(*s != '\0'){
+		s++;
+		i++;
+	}
+	return i;
+}
+
+int mon_strcmp(const char * s1, const char * s2){
+
+	int resultat = 0;
+
+	while(*s1 != '\0' && *s2 != '\0'){
+
+		if(*s1 > *s2){
+			return 1;
+		}
+		if(*s1 < *s2){
+			return -1;
+		}
+
+		s1++;
+		s2++;
+	}
+
+	return resultat;
+}
+
+int main(){
+
+	char mot[]="bonjour";
+
+	char *mot2 = "amour";
+
+	printf("Exercice 1:\nLa longueur du tableau est %d\n",mon_strlen(mot));
+
+	printf("\nExercice 2:\nLa longueur du tableau (version pointeur) est %d\n",mon_strlen2(mot2));
+
+	char *mot3 = "bonjour";
+
+	printf("\nExercice 3:\nLe resultat de la comparaison est %d\n",mon_strcmp(mot2,mot3));
+
+	return 0;
+}
