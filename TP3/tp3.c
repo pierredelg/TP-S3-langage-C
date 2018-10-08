f94ba6327b1f6bcdb994fc115d4390fa0d5c823c
Pierre DELGRANGE <delgranp@saule20.iut-infobio.priv.univ-lille1.fr>
2018-10-08 17:33:59 +0200

    1

--- TP3/tp3.c
+++ TP3/tp3.c
@@ -15,7 +15,7 @@ int mon_strlen2(const char *s){
 
 	int i = 0;
 
-	while(*s != '\0'){
+	while(*s != '\0'){  // for(i=0; *s; s++, i++) {}
 		s++;
 		i++;
 	}
@@ -28,27 +28,25 @@ int mon_strcmp(const char * s1, const char * s2){
 
 	while(*s1 != '\0' && *s2 != '\0'){
 
-		char s3 = *s1;
-		char s4 = *s2;
-
-		if (s3 >= 97 && s3 <= 122){
-			s3 = s3 - 32;
-		}
-		if (s4 >= 97 && s4 <= 122){
-			s4 = s4 - 32;
-		}
-
-		if(s3 > s4){
+		if(*s1 > *s2){//si s1 == s2 s1++ s2++
+			//sinon  return *s1 -*s2}
 			return 1;
+		
 		}
-		if(s3 < s4){
+		if(*s1 < *s2){
 			return -1;
 		}
 
 		s1++;
 		s2++;
 	}
-
+	//return *s1-*s2;
+	if(*s1 == '\0'){
+		return -1;
+	}
+	if(*s2 == '\0'){
+		return 1;
+	}
 	return resultat;
 }
 
@@ -59,27 +57,22 @@ int mon_strncmp(const char * s1, const char * s2, int n){
 
 	for(i = 0 ; i < n && *s1 != '\0' && *s2 != '\0';i++){
 
-		char s3 = *s1;
-		char s4 = *s2;
-
-		if (s3 >= 97 && s3 <= 122){
-			s3 = s3 - 32;
-		}
-		if (s4 >= 97 && s4 <= 122){
-			s4 = s4 - 32;
-		}
-
-		if(s3 > s4){
+		if(*s1 > *s2 ){
 			return 1;
 		}
-		if(s3 < s4){
+		if(*s1 < *s2){
 			return -1;
 		}
 
 		s1++;
 		s2++;
 	}
-
+	if(*s1 == '\0'){
+		return -1;
+	}
+	if(*s2 == '\0'){
+		return 1;
+	}
 	return resultat;
 }
 char *mon_strcat(char *s1,const char *s2){
@@ -135,7 +128,7 @@ char *mon_strstr(char *haystack, char *needle){
 			//si la 2eme chaine est arrivée a la fin
 			if(*needle == '\0'){
 				//On remonte le pointeur a la premiere lettre trouvée
-				while(compteur != 0){
+				while(compteur != 0){  // haystack -= compteur;
 					compteur --;
 					haystack --;
 				}
@@ -151,6 +144,8 @@ char *mon_strstr(char *haystack, char *needle){
 }
 
 char *mon_strstr2(char *haystack, char *needle){
+
+	int compteur = 0;
 	
 	while(*haystack){
 
@@ -158,10 +153,11 @@ char *mon_strstr2(char *haystack, char *needle){
 			return haystack;	
 		}
 		else{
+			compteur++;
 			haystack++;
 		}
 	}
-
+	haystack -= compteur;
 	return NULL;
 }
 
@@ -178,6 +174,8 @@ int main(){
 	printf("\nExercice 2:\nLa longueur du tableau (version pointeur) est %d\n",mon_strlen2(mot2));
 
 	printf("\nExercice 3:\nLe resultat de la comparaison est %d\n",mon_strcmp(mot,mot2));
+
+	printf("\nExercice 3:\nLe resultat de la comparaison avec bon est %d\n",mon_strcmp("aaaaaaaaaaaaaa",mot));
 
 	printf("\nExercice 4:\nLe resultat de la comparaison jusqu'au caractere %d est %d\n",3,mon_strncmp(mot,mot2,3));
 
--- TP4/tp4.c
+++ TP4/tp4.c
@@ -5,17 +5,47 @@
 
 char * miroir (const char *s){
 
-	char * resultat = malloc(sizeof(s) * strlen(s));
-	
-	while(*s){
-	
-		*resultat = *s;
-		s++;
-		resultat++;
-	}
-	
-	*resultat = '\0';
+	char * resultat = malloc(sizeof(*s) * strlen(s));
+	int compteur = 0;
 
+	if(resultat != NULL){
+	
+		while(*s){
+	
+			*resultat = *s;
+			s++;
+			resultat++;
+			compteur++;
+		}
+	
+		*resultat = '\0';
+
+		while(compteur != 0){
+			s--;
+			compteur --;
+			resultat --;
+		}
+	}
+	return resultat;
+}
+
+char * saisie (){
+
+	char * resultat = NULL;
+	int taille = 1;
+	int compteur = 0;
+	char c = ' ';
+	
+	do{
+		resultat = malloc(sizeof(*resultat) * taille);
+		c = getchar();
+		*resultat = c;
+		taille++;
+		resultat++;
+		compteur ++;
+	}while(c != EOF);
+
+	resultat -= compteur;
 	return resultat;
 }
 
@@ -24,6 +54,8 @@ int main (){
 	char * t1 = "salut";
 
 	printf("%s\n",miroir(t1));
+
+	printf("%s\n",saisie());
 
 	return 0;
 }
