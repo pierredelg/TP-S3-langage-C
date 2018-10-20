63471137f9ad59dacad8a2387f21a3afe6c6fc18
Pierre DELGRANGE <pierre.delgrange@etudiant.univ-lille1.fr>
2018-10-20 14:03:09 +0200

    tp4 ex2

--- TP4/chaine.c
+++ TP4/chaine.c
@@ -53,15 +53,44 @@ char * saisie (){
 	return resultat;
 }
 
+int main(int argc, char *argv[]){
 
+	int i = 1;
+	int bmirroir = 0;
+	int bscan = 0;
+	char *mot = NULL;
 
-int main (){
+	if(argc == 2 || argc == 3){
+		
+		if(argv[1][0] == '-'){
+			
+			while(argv[1][i]){
 
-	char * t1 = "salut";
+				if(argv[1][i] == 'm'){
+					bmirroir = 1;
+				}
+				if(argv[1][i] == 's'){
+					bscan = 1;
+				}
+				i++;
+			}
 
-	printf("%s\n",miroir(t1));
-
-	printf("%s\n",saisie());
-
+			if(bscan == 1 && argc == 2 && bmirroir == 0){
+				printf("%s\n",saisie());
+				return 0;
+			}
+			if(bmirroir == 1 && argc == 3 && bscan == 0){
+				printf("%s\n",miroir(argv[2]));
+				return 0;
+			}
+			if(bmirroir == 1 && bscan == 1 && argc == 2){
+				printf("%s\n",miroir(saisie()));
+				return 0;
+			}
+		}
+	}
+	
+	printf("mauvaise utilisation\n");
+	
 	return 0;
 }
